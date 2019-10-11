789cf1b889a5c59d00f6f7b39d9f16021ce45af8
SwampThingPaul <pjulian@ufl.edu>
2019-10-11 09:15:12 -0400

    edits to DBHYDRO_breakpoint

--- R/DBHYDRO_breakpoint.R
+++ R/DBHYDRO_breakpoint.R
@@ -12,19 +12,20 @@
 #' edate=as.Date("2001-06-01");
 #' dat=DBHYDRO_breakpoint(SDATE,EDATE,"AI516")
 DBHYDRO_breakpoint=function(SDATE,EDATE,DBK,col.names=c("DATETIME","Station","DBKEY","Data.Value","Flag","Comment")){
-  .Deprecated("SFWMD.DBHYDRO.Data.breakpoint")
+  require(RCurl)
   DBK.val=paste("",DBK,"",collapse="/",sep="")
   SDATE=paste(format(SDATE,"%Y"),toupper(format(SDATE,"%m")),format(SDATE,"%d"),sep="");#In YYYYMMDD format
   EDATE=paste(format(EDATE,"%Y"),toupper(format(EDATE,"%m")),format(EDATE,"%d"),sep="");#In YYYYMMDD format
   link=paste("http://my.sfwmd.gov/dbhydroplsql/web_io.report_process?v_period=uspec&v_start_date=",SDATE,"&v_end_date=",EDATE,"&v_report_type=format6&v_target_code=file_csv&v_run_mode=onLine&v_js_flag=Y&v_dbkey=",DBK.val,sep="")
-  REPORT=read.csv(link,skip=length(DBK)+2,col.names=col.names)[,1:6]
+  tmp=RCurl::getURL(link)
+  REPORT=read.csv(tmp,skip=length(DBK)+2,col.names=col.names,header=F)
   REPORT$DATETIME=as.POSIXct(REPORT$DATETIME,format="%d-%b-%Y %H:%M",tz="EST")
   REPORT$DATE=as.POSIXct(format(REPORT$DATETIME,format="%Y-%m-%d"),tz="EST")
   REPORT=subset(REPORT,is.na(DATETIME)==F)
   return(REPORT)
 }
 SFWMD.DBHYDRO.Data.breakpoint=function(SDATE,EDATE,DBK,col.names=c("DATETIME","Station","DBKEY","Data.Value","Flag","Comment")){
-  #.Deprecated("DBHYDRO_breakpoint")
+  #.Deprecated("SFWMD.DBHYDRO.Data.breakpoint")
   dat=DBHYDRO_breakpoint(SDATE,EDATE,DBK,col.names)
   warning("This function is being phased out. Consider using DBHYDRO_breakpoint in the future.")
   return(dat)
