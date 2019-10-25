f9cca69cbbd825fd813eb36df9cd61b7b681bf4d
SwampThingPaul <pjulian@ufl.edu>
2019-10-25 14:50:14 -0400

    added qq.function()

---
+++ R/qq.function.r
@@ -0,0 +1,17 @@
+#' Calculation of theortical quantile (Quantile-Quantile Plot)
+#'
+#' @param y data sample
+#' @export
+#' @return Part of the qqnorm generic function
+#' @examples
+#' set.seed(123)
+#' test=lm(rnorm(100)~rnorm(100)+100)
+#' qq.function(test$residuals)
+
+qq.function=function(y){
+  n=length(y)
+  r=order(order(y))
+  if(n>10){p=(r-1/2)/n}else{p=(r-3/8)/(n+1/4)}
+  qqnorm.val=qnorm(p)
+  return(qqnorm.val)
+}
