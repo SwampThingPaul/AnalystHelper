64492a49d7d1f78bb76c23b10e82936c8533e515
Paul Julian <SwampThingPaul@users.noreply.github.com>
2020-03-23 16:03:55 -0400

    more bandades....

--- R/conversion.R
+++ R/conversion.R
@@ -17,19 +17,30 @@
 #' \item{in.to.cm}
 #' \item{hPa.to.mmHg}
 #' }
-#' @export
 #' @examples
 #' cfs.to.m3d(350)
 
+#' @export
 cfs.to.m3d=function(x) x*2446.58
+#' @export
 cfs.to.km3d=function(x) x*2.44658e-6
+#' @export
 cfs.to.acftd=function(x) x*1.98347
+#' @export
 mt.to.kg=function(x) x*1000
+#' @export
 kg.to.mt=function(x) x/1000
+#' @export
 acres.to.m2=function(x)x*4046.86
+#' @export
 m3d.to.cfs=function(x) x/2446.58
+#' @export
 ft.to.m=function(x) x*0.3048
+#' @export
 m.to.ft=function(x) x*3.28084
+#' @export
 degF.to.C=function(x) x-32*(5/9)
+#' @export
 in.to.cm=function(x) x*2.54
+#' @export
 hPa.to.mmHg=function(x) x*0.75006157584566
