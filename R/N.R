350a96242fc910e1106575646c90fda8fbda3f80
Paul Julian <SwampThingPaul@users.noreply.github.com>
2020-04-20 20:29:10 -0400

    minor edit to add na.rm argument to N function

--- R/N.R
+++ R/N.R
@@ -1,7 +1,7 @@
 #' Returns sample size
 #'
 #' @param x numeric value
-#' @param NA.val the value to exclude, default is 'NA'
+#' @param na.rm a logical value indicating whether NA values should be stripped before the computation proceeds.
 #' @keywords count
 #' @export
 #' @return return the number of real values.
@@ -9,4 +9,9 @@
 #'example=c(rnorm(12),NA,NA,NA)
 #'N(example);# versus length(example)
 
-N=function(x,NA.val="NA") length(which(x!=NA.val))
+#N=function(x,NA.val="NA") length(which(x!=NA.val))
+#NA.val the value to exclude, default is 'NA'
+N=function(x,na.rm=FALSE){
+  ind <- is.na(x) | is.nan(x) | is.infinite(x)
+  return(length(x[!ind]))
+}
