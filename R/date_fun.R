33cb13e1cb0d9dc8d9bf5f87fef37e93cc4280c8
Paul Julian <pjulian@ufl.edu>
2024-06-14 16:12:35 -0400

    fixed date.fun to handle timezone conversion

--- R/date_fun.R
+++ R/date_fun.R
@@ -8,15 +8,31 @@
 #' @return Helper function to format date (and date-time) fields
 #' @examples
 #' datetime=as.character(c("2015-05-01 08:00:00","2015-10-15 12:00:00","2015-10-15 5:00:00"))
-#' date_fun(datetime);# just date
-#' date_fun(datetime, form="%F %X"); #include date and time
+#' date.fun(datetime);# just date
+#' date.fun(datetime, form="%F %X"); #include date and time
+#'
+#' date.fun("2020-05-01")
+#' date.fun(as.Date("2020-05-01"))
+#' date.fun(as.POSIXct("2020-05-01 12:00:00",tz="EST"),form="%F %H:%M:%S")
+#' date.fun(as.POSIXct("2020-05-01 12:00:00",tz="UTC"),form="%F %H:%M:%S")
 
 date_fun=function(x,tz="EST",form="%F"){
-  as.POSIXct(strptime(x,form),tz=tz)
+  # as.POSIXct(strptime(x,form),tz=tz)
+  date.fun(x,tz=tz,form=form)
 }
 
 #' @export date.fun
 date.fun=function(x,tz="EST",form="%F"){
-  #warning("This function has been replaced with date_fun")
-  as.POSIXct(strptime(x,form),tz=tz)
+  # Original code ... doesn't correct for changes in timezone
+  # as.POSIXct(strptime(x,form),tz=tz)
+  if(is.null(attr(x,"tzone"))&is.null(tz)){stop("Define Time Zone or set tz value")}
+  if(is.null(attr(x,"tzone"))&is.null(tz)==F){as.POSIXct(strptime(x,format=form),tz=tz)
+  }else if(attr(x,"tzone")!=tz){
+    attr(x,"tzone")<-tz
+    as.POSIXct(strptime(x,format=form),tz=tz)
+  }else{
+    as.POSIXct(strptime(x,format=form),tz=tz)
+  }
+
+
 }
